5604a61c1c87fe13bc7d2ba725065c5ca9569509
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2023-05-06 23:27:42 -0300

    feat: finish implementation of ProductsRepository with pg

--- src/api/v1/products/infra/data/products-repository/postgres/pg-products-repository.ts
+++ src/api/v1/products/infra/data/products-repository/postgres/pg-products-repository.ts
@@ -1,3 +1,4 @@
+import { Category } from '../../../../domain/entities/category.entity'
 import {
   ProductProps,
   Product,
@@ -15,6 +16,20 @@ export class PgProductsRepository
     CreateProductRepository,
     FindProductByIdRepository
 {
+  static toDomain(dbProduct, dbCategories): Product {
+    return Product.create({
+      id: dbProduct.id,
+      attributes: dbProduct.attributes,
+      brand: dbProduct.brand,
+      ean: dbProduct.ean,
+      title: dbProduct.title,
+      sellValue: dbProduct.sell_value,
+      costValue: dbProduct.cost_value,
+      categories: Array.isArray(dbCategories)
+        ? dbCategories.map(category => new Category(category))
+        : [],
+    })
+  }
   async create(product: ProductProps): Promise<Product> {
     const {
       id,
@@ -42,18 +57,64 @@ export class PgProductsRepository
           'INSERT INTO products_categories (product_id, category_id) VALUES ($1, $2)',
           [id, category.id]
         )
-      } 
+      }
     }
 
     return Product.create(product)
   }
 
-  findById(id: string): Promise<FindProductByIdRepository.Result> {
-    throw new Error('Method not implemented.')
+  async findById(id: string): Promise<FindProductByIdRepository.Result> {
+    const [product] = await db.query('SELECT * FROM products WHERE id = $1', [
+      id,
+    ])
+
+    if (!product) {
+      return null
+    }
+
+    const categories = await this.findProductCategories(id)
+    return PgProductsRepository.toDomain(product, categories)
   }
-  list(
+
+  async list(
     filters: ListProductsRepository.Params
   ): Promise<ListProductsRepository.Result> {
-    throw new Error('Method not implemented.')
+    const { attribute, brand, title } = filters
+    let where = '1=1\n'
+    let index = 1
+
+    if (attribute?.type && attribute?.value) {
+      where += `and attributes->>'${attribute.type}' = $${index++}\n`
+    }
+
+    if (brand) {
+      where += `and brand = $${index++}\n`
+    }
+
+    if (title) {
+      where += `and title like $${index++}\n`
+    }
+
+    const products = await db.query(
+      `SELECT * FROM products WHERE ${where}`,
+      [attribute, brand, title].filter(Boolean)
+    )
+
+    const parsedProduct: Product[] = []
+    for (const product of products) {
+      const productCategories = await this.findProductCategories(product.id)
+      parsedProduct.push(
+        PgProductsRepository.toDomain(product, productCategories)
+      )
+    }
+
+    return parsedProduct
+  }
+
+  private findProductCategories(id: string) {
+    return db.query(
+      'SELECT * FROM categories WHERE id IN (SELECT category_id FROM products_categories WHERE product_id = $1)',
+      [id]
+    )
   }
 }
