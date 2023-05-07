be79ce20dbcb5ef4c7d8529c1c3af17338fc29ec
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2023-05-06 21:33:13 -0300

    test(Produt): ensure GetProductById will search and return the product by id

---
+++ tests/api/v1/products/usecases/get-product-by-id.spec.ts
@@ -0,0 +1,28 @@
+import { beforeEach, describe, expect, test, vitest } from 'vitest'
+import { MockProxy, mock } from 'vitest-mock-extended'
+import { ProductRepository } from '../../../../../src/api/v1/products/domain/protocols/repositories/product.repository'
+import { mockProduct } from '../domain/entities/product.entity.mock'
+import { GetProductById } from '../../../../../src/api/v1/products/domain/usecases/get-product-by-id'
+
+describe('GetProductById', () => {
+  let productRepository: MockProxy<ProductRepository>
+  let sut: GetProductById
+
+  beforeEach(async () => {
+    productRepository = mock<ProductRepository>({
+      findById: vitest.fn().mockResolvedValue(mockProduct()),
+    })
+
+    sut = new GetProductById(productRepository)
+  })
+
+  test('should search a product by id', async () => {
+    const mockedProduct = mockProduct()
+    productRepository.findById.mockResolvedValueOnce(mockedProduct)
+    const params = mockedProduct.id
+    const result = await sut.execute(params)
+
+    expect(result).toEqual(mockedProduct)
+    expect(productRepository.findById).toHaveBeenCalledWith(params)
+  })
+})
